158935cd40a7eaf26b8495e43efe05a8b34abefe
thomas <trinn.tamas@webstar.hu>
2015-03-03 07:31:05 +0100

    default test

--- src/test/java/org/leanpoker/player/PlayerTest.java
+++ src/test/java/org/leanpoker/player/PlayerTest.java
@@ -1,7 +1,6 @@
 package org.leanpoker.player;
 
 import com.wcs.poker.gamestate.GameState;
-import com.wcs.poker.jsonconverter.JsonConverter;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,11 +9,9 @@ public class PlayerTest {
 
     @Test
     public void testBetRequest() throws Exception {
-        final String gameStateJson = "{\"key1\": \"value1\", \"key2\": \"value2\"}";
-        
-        JsonConverter<GameState> converter = new JsonConverter<>(GameState.class);
+        final GameState gameState = new GameState();
 
-        assertEquals(0, Player.betRequest(converter.fromJson(gameStateJson)));
+        assertEquals(0, Player.betRequest(gameState));
 
     }
 }
