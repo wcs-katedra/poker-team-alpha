7f2c176d440a832d09b84290399ee756ca0f256b
unknown <poker01@A109-01.itlab.pmmk.pte.hu>
2015-04-22 17:21:14 +0200

    04.22 Rank Enum

---
+++ src/main/java/com/wcs/poker/hand/Rank.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wcs.poker.hand;
+
+/**
+ *
+ * @author poker01
+ */
+public enum Rank {
+    A,K,Q,J,_10,_9,_8,_7,_6,_5,_4,_3,_2;
+}
