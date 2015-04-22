29f1e3a845fbe0de11725ac584ae0c5d5097404e
unknown <poker01@A109-01.itlab.pmmk.pte.hu>
2015-04-22 18:00:40 +0200

    #34 HandRankingService  Pair vizsgálat működik.

--- src/main/java/com/wcs/poker/hand/HandRankingService.java
+++ src/main/java/com/wcs/poker/hand/HandRankingService.java
@@ -1,6 +1,7 @@
 package com.wcs.poker.hand;
 
 import com.wcs.poker.gamestate.Card;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,8 +10,29 @@ import java.util.List;
  */
 public class HandRankingService {
 
-    public Hand evaulate(List<Card> loadCards) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    private List<Card> loadCards;
     
+    public Hand evaulate(List<Card> loadCards) {
+        this.loadCards=loadCards;
+        Hand hand = null;
+        if (isPair()) {
+            hand = new Hand(HandRank.PAIR, loadCards);
+        }
+        Collections.sort(loadCards);
+        loadCards.subList(0, 5);
+        return hand;
+    }
+
+    private boolean isPair() {
+        for (int i = 0; i < loadCards.size(); i++) {
+            Card card0 = loadCards.get(i);
+            for (int j = i + 1; j < loadCards.size(); j++) {
+                Card card1 = loadCards.get(j);
+                if (card0.isPair(card1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
